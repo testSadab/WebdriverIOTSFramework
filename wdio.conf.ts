3232a9f154b656db9efd4d27af9c5e64f90f54cd
Rohit <rk25037@gmail.com>
2021-09-10 22:50:27 +0530

    added report configuration

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -151,7 +151,18 @@ export const config: WebdriverIO.Config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec',['allure', {outputDir: 'allure-results'}]],
+    reporters: [
+        'spec',
+        [
+            'allure', 
+            {
+                outputDir: 'allure-results',
+                disableWebdriverStepsReporting: true,
+                disableWebdriverScreenshotsReporting: true,
+                useCucumberStepReporter: true
+            }
+        ]
+    ],
 
 
     //
