0a7bba8ee638b4117169d62167a801f455face90
MD SADAB SAQIB <66773365+testSadab@users.noreply.github.com>
2021-09-25 21:47:14 +0530

    Update wdio.conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -141,7 +141,7 @@ export const config: WebdriverIO.Config = {
     framework: 'cucumber',
     //
     // The number of times to retry the entire specfile when it fails as a whole
-    specFileRetries: 1,
+    specFileRetries: 0,
     //
     // Delay in seconds between the spec file retry attempts
     // specFileRetriesDelay: 0,
@@ -283,9 +283,9 @@ export const config: WebdriverIO.Config = {
      * @param {string}             result.error    error stack if scenario failed
      * @param {number}             result.duration duration of scenario in milliseconds
      */
-    afterStep: function (step, scenario, result) {
+    afterStep: async function (step, scenario, result) {
         if(!result.passed) {
-            browser.takeScreenshot()
+            await browser.takeScreenshot();
         }
     },
     /**
